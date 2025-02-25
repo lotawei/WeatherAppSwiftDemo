d3fcd4628a5ec7f827638165c60b4516012659a6
lotawei <1286594907@qq.com>
2025-02-25 22:27:05 +0800

    Initial Commit

---
+++ WeatherAppSwiftDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppSwiftDemo
+//
+//  Created by work on 2025/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherAppSwiftDemoTests/WeatherAppSwiftDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftDemoTests.swift
+//  WeatherAppSwiftDemoTests
+//
+//  Created by work on 2025/2/25.
+//
+
+import Testing
+@testable import WeatherAppSwiftDemo
+
+struct WeatherAppSwiftDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
